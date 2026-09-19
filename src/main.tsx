97df93140695ea380c13d1e2ae65a1c13eef0c3e
Patrik Norrgård <patrik.norrgard@yahoo.se>
2026-09-19 12:36:45 +0200

    add netlify.toml and remove vite base path

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/MovieBase">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
